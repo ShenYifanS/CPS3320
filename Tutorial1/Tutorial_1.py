64bcdc8ff424795bd4547dfa0dcb97dfbe1c4b0b
ShenYifanS <46106972+ShenYifanS@users.noreply.github.com>
2022-03-15 10:59:17 +0800

    Add files via upload

---
+++ Tutorial1/Tutorial_1.py
@@ -0,0 +1,13 @@
+"""
+Group: Zhang Yijie(Edward) 1098690 and Shen Yifan(Sivan) 1098328
+
+"""
+
+name = input("Please enter your name:")
+print("Hello, My name is: "+ name)
+age = input("Please enter your age:")
+print("I am "+ str(age) + " years")
+address = input("Please enter your address:")
+print("I live in "+ address)
+sport = input("Please enter your favorite sport:")
+print("I like to play " + sport)
